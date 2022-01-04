984202b955f95b50c0a2e7b6efc53e90f464acd6
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-04 21:34:46 +0530

    Create solution.c

---
+++ Logic-Based-Programming/LBP6/solution.c
@@ -0,0 +1,13 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%d",n+n/6);
+    return 0;
+}
