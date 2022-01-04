3a9deea796312a80c3cd62b20cdc9d73501a2440
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-04 20:48:37 +0530

    Create solution.c

---
+++ Logic-Based-Programming/LBP5/solution.c
@@ -0,0 +1,14 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    n++;
+    printf("%d",n);
+    return 0;
+}
