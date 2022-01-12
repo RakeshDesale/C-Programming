931ae457c1fb3c35b9fd47b394c4a898166d8e03
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-12 16:13:33 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-013/solution2.c
@@ -0,0 +1,15 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char n[20];
+    int c;
+    scanf("%s",n);
+    c=strlen(n);
+    printf("%d",c);
+    return 0;
+}
