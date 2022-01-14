bc45fd23b55255aece7e5c41f44faa8fa8a6909d
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-14 14:45:44 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-018/solution1.c
@@ -0,0 +1,19 @@
+# Without using Function:
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int day;
+    char month[10];
+    scanf("%s %d",month,&day);
+    if (strcmp(month,"july")==0 && day==5)
+        printf("1");
+    else
+        printf("0");
+    return 0;
+}
