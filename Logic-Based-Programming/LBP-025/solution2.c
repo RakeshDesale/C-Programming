bad0dd2fcec60b73408f60de89f99fc145612a47
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-15 18:04:49 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-025/solution2.c
@@ -0,0 +1,15 @@
+# Without Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int celsius,fahrenheit;
+    scanf("%d",&celsius);
+    printf("%d",fahrenheit=(celsius*1.8+32));
+    return 0;
+}
