0c2c57da8974545165683d1a6b1897ccb257e6c8
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-15 16:39:37 +0530

    Create solution.c

---
+++ Logic-Based-Programming/LBP-022/solution.c
@@ -0,0 +1,26 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,d,sum=0,temp=0;
+    scanf("%d",&n);
+    temp=n;
+    if(n>0){
+        while(n!=0){
+            d=n%10;
+            sum=sum+d;
+            n=n/10;
+        }
+        if(temp%sum==0)
+            printf("Yes");
+        else
+            printf("No");
+    }
+    else
+        exit(0);
+    return 0;
+}
