218c07745b9c4e13d82844f6f2cb0a5ebd5e3902
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-27 19:13:10 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-039/solution2.c
@@ -0,0 +1,32 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int BlackJack(int,int);
+
+int main() {
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    printf("%d",BlackJack(n1,n2));
+    return 0;
+}
+
+int BlackJack(int n1,int n2){
+    if (n1>21 && n2>21)
+        return 0;
+    else if(n1<=21 && n2>21)
+        return n1;
+    else if(n1>21 && n2<=21)
+        return n2;
+    else if(n1<=21 && n2<=21){
+        if (n1<n2)
+            return n2;
+        else
+            return n1;
+    }
+    else
+        return n1;
+}
