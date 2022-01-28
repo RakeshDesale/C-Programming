9d28ea982a69e06947a9ffe6bb9de161c6dd6f1f
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-28 18:55:56 +0530

    Update solution2.c

--- Logic-Based-Programming/LBP-045/solution2.c
+++ Logic-Based-Programming/LBP-045/solution2.c
@@ -1,4 +1,27 @@
-# Without Using Function
+# Optimized Solution:
 
-# Solution:
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int isperfect(int);
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+        printf((isperfect(n))?"true":"false");
+    else
+        exit(0);
+    return 0;
+}
+
+int isperfect(int n){
+    int i,sum=0;
+    for(i=1;i<n;i++){
+        if(n%i==0)
+            sum=sum+i;
+    }
+    return (sum==n)?1:0;
+}
