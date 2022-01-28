f21cc5cc912e63b4a0250ad468a53a1e8fdfa98e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-28 14:47:17 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-040/solution1.c
@@ -0,0 +1,18 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,d,rev=0;
+    scanf("%d",&n);
+    while(n!=0){
+        d=n%10;
+        rev=rev*10+d;
+        n=n/10;
+    }
+    printf("%d",rev);
+    return 0;
+}
