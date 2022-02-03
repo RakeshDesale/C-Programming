8cdf772dbe2faa18f4309e288965bbfab39c0ac2
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-03 14:55:54 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-056/solution2.c
@@ -0,0 +1,15 @@
+# Without Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char str1[30],str2[30];
+    scanf("%s %s",str1,str2);
+    printf("%s %s",str2,str1);
+    return 0;
+}
