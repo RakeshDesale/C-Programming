33ddb416e19f78f6fd05379ca5f722deeabcebbf
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-08 19:24:23 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-065/solution2.c
@@ -0,0 +1,17 @@
+# Without Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char str[20];
+    scanf("%s",str);
+    printf("%c%c...",str[0],str[1]);
+    printf("%c%c...",str[0],str[1]);
+    printf("%s?", str);
+    return 0;
+}
