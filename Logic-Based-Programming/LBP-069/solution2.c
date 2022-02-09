77127207d8ea46ffd82be14964536aac1532747b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-09 15:28:35 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-069/solution2.c
@@ -0,0 +1,21 @@
+# Without Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char str[20];
+    int i;
+    scanf("%s",str);
+    for(i=0;str[i];i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            continue;
+        else
+          printf("%c",str[i]);
+    }
+    return 0;
+}
