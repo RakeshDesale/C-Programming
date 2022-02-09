e08a517298b4fd5234cfd7ff2c9a4aac1df05c2e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-09 15:29:21 +0530

    Create solution3.c

---
+++ Logic-Based-Programming/LBP-069/solution3.c
@@ -0,0 +1,19 @@
+# Without Using Function and Reverse Condition
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char str[20];
+    int i;
+    scanf("%s",str);
+    for(i=0;str[i];i++){
+        if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u')
+            printf("%c",str[i]);
+    }
+    return 0;
+}
