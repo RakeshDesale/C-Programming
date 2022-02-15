4864c834a8cf9296b06b509bb2c4c37d485a0a97
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-15 14:58:35 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-082/solution2.c
@@ -0,0 +1,24 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void nextLetter(char []);
+
+int main() {
+    char str[20];
+    scanf("%s",str);
+    nextLetter(str);
+    return 0;
+}
+
+void nextLetter(char str[]){
+    int i;
+    for(i=0;str[i];i++){
+        if(str[i]!='z')
+            str[i]=str[i]+1;
+    }
+    printf("%s",str);
+}
