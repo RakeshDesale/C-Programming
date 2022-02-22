6ffc814d68f17a9d3853b50f5aa4a17d69518370
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-22 16:44:27 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-096/solution2.c
@@ -0,0 +1,26 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int vowelCount(char []);
+
+int main() {
+    char str[20];
+    scanf("%s",str);
+    printf("%d",vowelCount(str));
+    return 0;
+}
+
+int vowelCount(char str[]){
+    int i,count=0;
+    for(i=0;str[i];i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            count++;
+    }
+    return count;
+}
