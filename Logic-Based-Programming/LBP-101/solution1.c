08352c893a7060507656be3b196c0ce25ccdde19
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-27 20:06:07 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-101/solution1.c
@@ -0,0 +1,16 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d ",&arr[i]);
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
