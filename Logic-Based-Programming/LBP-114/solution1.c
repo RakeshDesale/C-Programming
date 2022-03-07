c4fb3b345c6a650953cdf3ed3959610e736699b1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-07 21:46:06 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-114/solution1.c
@@ -0,0 +1,24 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,i,j,min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                min=arr[i];
+                arr[i]=arr[j];
+                arr[j]=min;
+            }
+        }
+    }
+    printf("%d",arr[0]);
+    return 0;
+}
