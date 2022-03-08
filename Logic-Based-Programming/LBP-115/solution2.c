ab0c181e9227935395b316eccf87c5e007dff783
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-08 21:35:42 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-115/solution2.c
@@ -0,0 +1,29 @@
+# Different Approach
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,i,j,temp=0,min=0,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]<arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    min=arr[0];
+    for(i=0;i<n;i++)
+        max=arr[i];
+    printf("%d",abs(min-max));
+    return 0;
+}
