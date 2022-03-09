707956e7ebc19eaa1a8c5dd22f4d1223b7d03451
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-09 21:33:58 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-116/solution2.c
@@ -0,0 +1,33 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int secondMax(int [], int);
+
+int main() {
+    int arr[99],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    printf("%d",secondMax(arr,n));
+    return 0;
+}
+
+int secondMax(int arr[], int n){
+    int i,j,temp;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return (arr[n-2]);
+}
