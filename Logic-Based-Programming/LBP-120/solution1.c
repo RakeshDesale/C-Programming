346ad7079b3f0d4fceacb51b18367c5f8929e481
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-14 22:09:17 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-120/solution1.c
@@ -0,0 +1,19 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,ele,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    scanf("%d",&ele);
+    arr[n]=ele;
+    n++;
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
