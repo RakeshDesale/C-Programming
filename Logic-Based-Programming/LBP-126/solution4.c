d5e9c1ac9de2dfd3bbd414d413508701ea0de14a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-17 21:05:38 +0530

    Create solution4.c

---
+++ Logic-Based-Programming/LBP-126/solution4.c
@@ -0,0 +1,33 @@
+# Another Approach Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void updateElementOnPosition(int [], int, int, int);
+
+int main() {
+    int arr[99],n,loc,ele,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    scanf("%d",&loc);
+    scanf("%d",&ele);
+    updateElementOnPosition(arr,n,loc,ele);
+    return 0;
+}
+
+void updateElementOnPosition(int arr[], int n, int loc, int ele){
+    int i;
+    for(i=0;i<n;i++){
+        if(i==loc){
+            arr[i] = ele;
+            break;
+        }
+    }
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+}
