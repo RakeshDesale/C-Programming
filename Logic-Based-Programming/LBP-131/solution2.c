f57b0343593db07b3534e9f5a3edf2c6ae6865c1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-24 20:34:46 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-131/solution2.c
@@ -0,0 +1,34 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void arrSort(int [], int);
+
+int main() {
+    int arr[99],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    arrSort(arr,n);
+    return 0;
+}
+
+void arrSort(int arr[], int n){
+    int i,j,temp;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+}
