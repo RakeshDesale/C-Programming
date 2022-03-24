28211ff6a76fd9e070f5fea9e60ec3d57be8d470
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-24 20:34:02 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-131/solution1.c
@@ -0,0 +1,25 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
