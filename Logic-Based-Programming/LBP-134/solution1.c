8cef3240a73b3e328a92de3cc4783f29c651f1d2
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-25 21:08:08 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-134/solution1.c
@@ -0,0 +1,19 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[99],n,i,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        if(i%2==0)
+            sum = sum+arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
