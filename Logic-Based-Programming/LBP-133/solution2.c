05a66c03cc54c789f2e759f4e4f21418ca603e45
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-25 20:53:34 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-133/solution2.c
@@ -0,0 +1,20 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a[99],b[99],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    for(i=0;i<n;i++)
+        a[i] = a[i]+b[i];
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    return 0;
+}
