f02df441be34c25eb70b68ca190ca3ab907f34d6
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-30 20:45:11 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-141/solution1.c
@@ -0,0 +1,30 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[100],n,i,j,temp,low,high;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    low=0;
+    high=n-1;
+    while(low<=high){
+        printf("%d %d ",arr[low],arr[high]);
+        low++;
+        high--;
+    }
+    return 0;
+}
