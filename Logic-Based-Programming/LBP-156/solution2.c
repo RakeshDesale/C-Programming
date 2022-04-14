8cc9b6a6e3d0c1c339130c8ad8162b3ea1176330
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-14 16:28:10 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-156/solution2.c
@@ -0,0 +1,31 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void indexChar(char [], int [], int);
+
+int main() {
+    char str[100];
+    int arr[100],n,i;
+    scanf("%[^\n]s",str);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    indexChar(str,arr,n);
+    return 0;
+}
+
+void indexChar(char str[], int arr[], int n){
+    int i;
+    for(i=0;str[i];i++){
+        if(str[i]>='A' && str[i]<='Z')
+            str[i] = str[i] + 32;
+    }
+    for(i=0;i<n;i++)
+        printf("%c",str[arr[i]]);
+}
