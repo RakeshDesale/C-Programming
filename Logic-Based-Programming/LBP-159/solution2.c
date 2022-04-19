02c6bd6b391c0d4a08e14628daa5476ecf6033a4
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-19 16:48:10 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-159/solution2.c
@@ -0,0 +1,23 @@
+# Without Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[100],n,i,flag = 1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){
+        if(arr[i]==0){
+            flag = 0;
+            break;
+        }
+    }
+    printf((flag)?"true":"false");
+    return 0;
+}
