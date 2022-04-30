c8b92d474cc0b076965dcc428fa784446f01ccb7
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-30 16:01:24 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-176/solution1.c
@@ -0,0 +1,17 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1, num2, i, gcd;
+    scanf("%d %d",&num1, &num2);
+    for(i=1;i<=num1 && i<=num2;i++){
+        if(num1%i==0 && num2%i==0)
+            gcd = i;
+    }
+    printf("%d",gcd);
+    return 0;
+}
