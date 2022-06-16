6383115ff8c3f3d81381abb4f5a7d9f2b9cb2a9a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-16 12:16:24 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-188/solution2.c
@@ -0,0 +1,29 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,d,sum;
+    scanf("%d",&n);
+    while(1){
+        if(n >= 0 && n <= 9){
+            printf("%d",n);
+            break;
+        }
+        else{
+            sum = 0;
+            while(n != 0){
+                d = n % 10;
+                sum = sum + d;
+                n = n / 10;
+            }
+            n = sum;
+        }
+    }
+    return 0;
+}
