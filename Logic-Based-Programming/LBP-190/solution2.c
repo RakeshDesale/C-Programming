52862a2e164fd63b605ba4e57d5397a046dfb4a7
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-17 12:59:15 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-190/solution2.c
@@ -0,0 +1,35 @@
+# Using abs Method
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,arr[100],i = 0,j,temp,len,flag = 1;
+    scanf("%d",&n);
+    while(n != 0){
+        arr[i++] = n%10;
+        n = n/10;
+    }
+    len = i;
+    for(i=0;i<len;i++){
+        for(j=i+1;j<len;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<len-1;i++){
+        if(abs(arr[i] - arr[i+1])>2){
+            flag = 0;
+            break;
+        }
+    }
+    printf((flag==1)?"Yes":"No");
+    return 0;
+}
