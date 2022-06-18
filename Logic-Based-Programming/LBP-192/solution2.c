b3ced070dd93efa5ef9e3c3ccb4727ba12543678
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-18 14:57:48 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-192/solution2.c
@@ -0,0 +1,30 @@
+# Using Inbuilt pow Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int sum(int);
+
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    for(i=2;i<=n;i++){
+        if(i == sum(i))
+            printf("%d ",i);
+    }
+    return 0;
+}
+
+int sum(int n){
+    int d,sum = 0;
+    while(n != 0){
+        d = n % 10;
+        sum = sum + (int)pow(d,3);
+        n = n / 10;
+    }
+    return sum;
+}
