db326097d010f33885946072ee6e4bdd57dfe693
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-23 20:24:27 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-199/solution1.c
@@ -0,0 +1,18 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,arr[100],k,i,sum = 0;
+    scanf("%d",&n);
+    for(i = 0;i < n;i++)
+        scanf("%d",&arr[i]);
+    scanf("%d",&k);
+    for(i = 0;i < n;i++)
+        sum = sum + (arr[i]/k);
+    printf("%d",sum);
+    return 0;
+}
