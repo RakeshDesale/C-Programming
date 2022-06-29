367fbc065810d6b3eab8d3bea4376ef198c3f630
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-29 17:55:29 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-206/solution1.c
@@ -0,0 +1,21 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], i, j, sum;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    for(i = 0; i < 3; i++){
+        sum = 0;
+        for(j = 0; j < 3; j++)
+            sum = sum + arr[i][j];
+        printf("%d\n",sum);
+    }
+    return 0;
+}
