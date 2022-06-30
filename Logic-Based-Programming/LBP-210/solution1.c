fc48056c3d40253c0cfa2434d29090aa8a0f6834
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-30 16:25:11 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-210/solution1.c
@@ -0,0 +1,16 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    printf("%d",arr[0][0] + arr[2][2]);
+    return 0;
+}
