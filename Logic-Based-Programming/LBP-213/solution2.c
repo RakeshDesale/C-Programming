377999c734684ea951452ff96a569552f1ef29ae
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-05 17:27:15 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-213/solution2.c
@@ -0,0 +1,29 @@
+# Another Approach
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], i, j, k, temp;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            for(k = j + 1; k < 3; k++){
+                if(arr[i][j] > arr[i][k]){
+                    temp = arr[i][j];
+                    arr[i][j] = arr[i][k];
+                    arr[i][k] = temp;
+                }
+            }
+        }
+    }
+    printf("%d",arr[2][2]);
+    return 0;
+}
