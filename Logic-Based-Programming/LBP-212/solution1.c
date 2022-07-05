27f4c8fa30c27f23f65ec4e5710647f803533966
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-05 17:19:11 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-212/solution1.c
@@ -0,0 +1,18 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], i, j, prod = 1;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    for(i = 0; i < 3; i++)
+        prod = prod * arr[i][3-i-1];
+    printf("%d",prod);
+    return 0;
+}
