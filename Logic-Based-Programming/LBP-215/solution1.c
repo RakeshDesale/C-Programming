504c79cb8fe4fd1f5118e31ca72d7483015eded7
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-06 17:15:41 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-215/solution1.c
@@ -0,0 +1,23 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], i, j, max;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    max = arr[0][0];
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(max < arr[i][j])
+                max = arr[i][j];
+        }
+        printf("%d\n",max);
+    }
+    return 0;
+}
