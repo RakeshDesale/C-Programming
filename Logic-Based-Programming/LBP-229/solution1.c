bbf133fb7c446765654cbd4e7ee3a41d60966f02
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-15 16:16:29 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-229/solution1.c
@@ -0,0 +1,35 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a[3][3], b[3][3], i, j, temp, k;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&a[i][j]);
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            b[i][j] = a[j][i];
+    }    
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            for(k = j + 1; k < 3; k++){
+                if(b[i][j] > b[i][k]){
+                    temp = b[i][j];
+                    b[i][j] = b[i][k];
+                    b[i][k] = temp;
+                }
+            }
+        }
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%d ",b[j][i]);
+        printf("\n");
+    }
+    return 0;
+}
