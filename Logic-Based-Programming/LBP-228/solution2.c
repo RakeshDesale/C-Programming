1098f61745cf0064f4e7969454b7b618ba9067d5
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-15 16:04:25 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-228/solution2.c
@@ -0,0 +1,40 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void sortRowEleDesc(int [3][3]);
+
+int main() {
+    int arr[3][3], i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    sortRowEleDesc(arr);
+    return 0;
+}
+
+void sortRowEleDesc(int arr[3][3]){
+    int i, j, k, temp;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            for(k = j + 1; k < 3; k++){
+                if(arr[i][j] < arr[i][k]){
+                    temp = arr[i][j];
+                    arr[i][j] = arr[i][k];
+                    arr[i][k] = temp;
+                }
+            }
+        }
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%d ",arr[i][j]);
+        printf("\n");
+    }
+}
