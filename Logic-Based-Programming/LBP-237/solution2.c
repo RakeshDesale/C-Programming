2a521abd8a03da6a36d3856e2d9e386f1a84e03b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-27 16:11:25 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-237/solution2.c
@@ -0,0 +1,21 @@
+# Optimized Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[3][3], m, i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    scanf("%d",&m);
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%d ",arr[i][j] * m);
+        printf("\n");
+    }
+    return 0;
+}
