660e1fa069f70b01ed41755bb61db7151b1b4270
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-27 16:23:09 +0530

    Create solution1.c

---
+++ Logic-Based-Programming/LBP-238/solution1.c
@@ -0,0 +1,28 @@
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a[3][3], b[3][3], i, j, flag = 1;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&a[i][j]);
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            b[i][j] = a[j][i];
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(a[i][j] != b[i][j]){
+                flag = 0;
+                break;
+            }
+        }
+    }
+    printf((flag == 1)?"Yes":"No");
+    return 0;
+}
