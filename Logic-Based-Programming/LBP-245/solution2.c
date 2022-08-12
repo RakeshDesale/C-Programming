031edd672d0f31cb32195748fc41f08ee51c3f26
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-12 15:37:14 +0530

    Create solution2.c

---
+++ Logic-Based-Programming/LBP-245/solution2.c
@@ -0,0 +1,31 @@
+# Using Function
+
+# Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int sumOfEvenIndexedRowCol(int [3][3]);
+
+int main() {
+    int arr[3][3], i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    printf("%d",sumOfEvenIndexedRowCol(arr));
+    return 0;
+}
+
+int sumOfEvenIndexedRowCol(int arr[3][3]){
+    int i, j, sum = 0;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if((i + j) % 2 == 0)
+                sum = sum + arr[i][j];
+        }
+    }
+    return sum;
+}
