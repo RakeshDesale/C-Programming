be159ce869f2cd3e42906ad70ca36c94ae593e02
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-31 17:31:34 +0530

    Create solution3.c

---
+++ Logic-Based-Programming/LBP-249/solution3.c
@@ -0,0 +1,35 @@
+# Using Function
+
+# Optimized Solution:
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int rev(int);
+
+int main() {
+    int arr[3][3], i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            scanf("%d",&arr[i][j]);
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ",rev(arr[i][j]));
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int rev(int n){
+    int r = 0, d;
+    while(n != 0){
+        d = n % 10;
+        r = r * 10 + d;
+        n = n / 10;
+    }
+    return r;
+}
